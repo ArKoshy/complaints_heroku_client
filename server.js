7e972f601d7487dde3cf7836b1aa6e2ff8c00b5d
ArKoshy <arkoshy@gmail.com>
2018-10-28 01:45:26 +0530

    fixed path to index file2

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-app.use(express.static('./dist/myappname'));
+app.use(express.static('./dist/complaintsUI'));
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('./dist/complaintsUI/index.html'));
+  res.sendFile(path.join(__dirname +'/dist/complaintsUI/index.html'));
 });
 app.listen(process.env.PORT || 8080);
